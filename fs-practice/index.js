ae6d9e1dbedb1b70533e09ae0e51ee807db15495
Karam Abbas <kma69362@gmail.com>
2025-08-21 14:59:50 -0700

    commit

---
+++ fs-practice/index.js
@@ -0,0 +1,26 @@
+// Reading files:
+
+// Async
+const {readFile, writeFile} = require('fs')
+
+readFile('./notes.txt', 'utf8', (err, firstData) =>{
+    if(err){console.log(err);return}
+    else{console.log(firstData, "Asynchronous File read successfully")}
+})
+
+writeFile('./asyncOutput.txt', 'Written with fs.writeFile (asynchronous).', (err) =>{
+    if(err){console.log(err);return}
+})
+
+writeFile('./notes.txt', ' New line', {flag:'a'}, (err)=>{
+    if(err){console.log(err);return}
+})
+
+// Sync
+const {readFileSync, writeFileSync} = require('fs')
+const path = require('path')
+
+const notes = readFileSync(path.join(__dirname, './notes.txt'), 'utf8')
+console.log(notes)
+
+writeFileSync(path.join(__dirname, './syncOutput.txt'), 'Written with fs.fileWriteSync(synchronous).', {flag:'w'})
