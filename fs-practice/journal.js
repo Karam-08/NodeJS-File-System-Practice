9748e1cab36fba383ff01328d1eec33b4772075d
Karam Abbas <kma69362@gmail.com>
2025-08-22 15:26:26 -0700

    installed prompt-sync

--- fs-practice/journal.js
+++ fs-practice/journal.js
@@ -1,20 +1,3 @@
-const {readFile, writeFile} = require('fs')
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question('Give an input for the journal: ', (input) =>{
-    writeFile('./journal.txt', input, {flag:'a'}, (err)=>{
-        if(err){console.log(err);return}
-        else{console.log("Your input has been stored into the journal.")}
-    })
-})
-
-// TESTING
-// rl.question('What is your name? ', (name) =>{
-//     console.log(`Hello, ${name}!`);
-//     rl.close();
-// });
+const prompt = require('prompt-sync')();
+const name = prompt('What is your name? ');
+console.log(`Hello, ${name}!`);
